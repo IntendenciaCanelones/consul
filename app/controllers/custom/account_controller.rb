fc6967d681d3b5ee62eb1c275c2cfab76ca8cf94
GobiernoCanelones <86753907+GobiernoCanelones@users.noreply.github.com>
2022-07-15 14:16:16 -0300

    Modificaciones de los controllers
    Para agregar barrio al formulario de registro, cuenta y para hacer la notificación del voto.

---
+++ app/controllers/custom/account_controller.rb
@@ -0,0 +1,38 @@
+class AccountController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_account
+  load_and_authorize_resource class: "User"
+
+  def show
+   geo = @account.geozones_area_id
+   if geo.blank?
+   flash[:alert] = "Debe seleccionar el barrio para continuar"
+  end
+ end
+  def update
+    if @account.update!(account_params)
+      redirect_to account_path, notice: t("flash.actions.save_changes.notice")
+    else
+      @account.errors.messages.delete(:organization)
+      render :show
+    end
+  end
+
+  private
+
+    def set_account
+      @account = current_user
+    end
+
+    def account_params
+      attributes = if @account.organization?
+                     [:phone_number, :email_on_comment, :email_on_comment_reply, :newsletter,
+                      organization_attributes: [:name, :responsible_name]]
+                   else
+                     [:username, :public_activity, :public_interests, :email_on_comment,
+                      :email_on_comment_reply, :email_on_direct_message, :email_digest, :newsletter,
+                      :official_position_badge, :recommended_debates, :recommended_proposals, :geozones_area_id]
+                   end
+      params.require(:account).permit(*attributes)
+    end
+end
--- app/controllers/custom/users/registrations_controller.rb
+++ app/controllers/custom/users/registrations_controller.rb
@@ -62,7 +62,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
       params[:user].delete(:redeemable_code) if params[:user].present? && params[:user][:redeemable_code].blank?
       params.require(:user).permit(:username, :email, :password,
                                    :password_confirmation, :terms_of_service, :locale,
-                                   :redeemable_code, :document_number, :date_of_birth, :gender, :cif, :domicilio, :geozone_id)
+                                   :redeemable_code, :document_number, :date_of_birth, :gender, :cif, :domicilio, :geozone_id, :geozones_area_id)
     end
 
     def configure_permitted_parameters
